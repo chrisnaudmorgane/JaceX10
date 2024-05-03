0b29ed26c6df2037495c4e236c0c32e1a8b7e124
Chrisnaud <146475708+chrisnaudmorgane@users.noreply.github.com>
2024-05-03 14:06:14 +0100

    make some function

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,29 +1,81 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Portofilieul',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Investissement'),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: Text(
-            "Chrisnaud",
-          style: TextStyle(
-            fontSize: 14,
-            fontFamily: 'Poppins'
+      title: 'Task Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Task Manager'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> tasks = [];
+  final TextEditingController _taskController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(tasks[index]),
+                );
+              },
+            ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _taskController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter a task',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      tasks.add(_taskController.text);
+                      _taskController.clear();
+                    });
+                  },
+                  icon: const Icon(Icons.add),
+                ),
+              ],
+            ),
           ),
-        ),
-      )
+        ],
+      ),
     );
   }
 }
